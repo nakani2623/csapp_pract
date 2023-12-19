0f57e16c41a8588dabc3c15295164f79fa00e3fb
zsun2623 <zsun2623@uni.sydney.edu.au>
2023-12-19 16:07:33 +1100

    created branch for macOS, binary executable for macOS

--- 2/show_data.c
+++ 2/show_data.c
@@ -17,8 +17,12 @@ void show_long(long data) {
     show_bytes((byte_pointer) &data, sizeof(long));
 }
 
-void show_double(short data) {
+void show_double(double data) {
     show_bytes((byte_pointer) &data, sizeof(double));
+}
+
+void show_int(int data) {
+    show_bytes((byte_pointer) &data, sizeof(int));
 }
 
 int main() 
@@ -26,9 +30,21 @@ int main()
     short i = 25;
     long j = 1321403242343;
     double k = 0.00004;
+    int a = 1;
+    int b = -1;
 
-    show_double(k);
-    show_short(i);
-    show_long(j);
+    // show_double(k);
+    // show_short(i);
+    // show_long(j);
+
+    show_int(a);
+    show_double((double) a);
+
+    show_int(b);
+    show_double((double) b);
+
+    printf("print int a as float 1: %f\n", (double) a);
+
+
     return 0;
 }
